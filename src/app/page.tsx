fa35050b3cc6b9cfd23f6ee1774cf5a5b7c4c16d
issamedine <b.issamedine@gmail.com>
2024-08-14 22:44:57 +0100

    check integrate signin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 
+import useRefreshToken from '@/hooks/useRefreshToken';
 import { useSession, signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 export default function HomePage() {
-  const { data: session } = useSession();
+  const router = useRouter();
+  const code = router.query.code;
+  console.log("🚀 ~ HomePage ~ code:", code)
+  useRefreshToken(code as string);
 
   return (
     <div>
       <h1>Welcome to Spotify App</h1>
-      {session ? (
+      {code ? (
         <div>
-          <p>Signed in as {session.user?.name}</p>
-          <button onClick={() => signOut()}>Sign out</button>
+          <p>Signed in as </p>
+          <button>Sign out</button>
         </div>
       ) : (
         <a href="/signin">Sign in with Spotify</a>
