ab2a2ff047193890a0aac55b011deda58f385c41
issamedine <b.issamedine@gmail.com>
2024-08-14 21:22:22 +0100

    check integrate signin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import AuthButton from "@/components/auth/AuthButton";
 import UserProfile from "@/components/profile/UserProfile";
+import useRefreshToken from "@/hooks/useRefreshToken";
+import { useRouter } from "next/router";
 
 export default function HomePage() {
+  const router = useRouter();
+
+  const code = router.query.code;
+
+  useRefreshToken(code as string);
+
   return (
     <div>
       <h1>Spotify OAuth with NextAuth.js</h1>
-      <AuthButton />
-      <UserProfile />
+      {code ? <UserProfile /> : <AuthButton />}
     </div>
   );
 }
--- src/components/auth/AuthButton.tsx
+++ src/components/auth/AuthButton.tsx
@@ -1,19 +1,12 @@
 'use client';
 
 import { authorize } from '@/API/authorize';
+import useRefreshToken from '@/hooks/useRefreshToken';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import { useRouter } from "next/router";
 
 export default function AuthButton() {
-  const { data: session } = useSession();
 
-  if (session) {
-    return (
-      <>
-        <p>Signed in as {session.user?.email}</p>
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
   return (
     <button onClick={authorize}>Sign in with Spotify</button>
   );
