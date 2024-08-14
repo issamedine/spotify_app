a3b873ad716fd0e985b2483158400071d8eabcda
issamedine <b.issamedine@gmail.com>
2024-08-14 21:46:13 +0100

    check integrate signin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,14 @@ import AuthButton from "@/components/auth/AuthButton";
 import UserProfile from "@/components/profile/UserProfile";
 import useRefreshToken from "@/hooks/useRefreshToken";
 import { useRouter } from "next/navigation";
-import { useSearchParams } from 'next/navigation';
 
 export default function HomePage() {
   const router = useRouter();
-  console.log("🚀 ~ HomePage ~ router:", router);
-
-  const searchParams = useSearchParams();
-
-  const code = searchParams.get('code') ?? 'Not provided';
-  // const code = router.code;
-  // useRefreshToken(code as string);
+  const { code } = router as { code?: string };
+  useRefreshToken(code as string);
   // const code = router.query.code;
 
-  useRefreshToken(code as string);
+  // useRefreshToken(code as string);
 
   return (
     <div>
